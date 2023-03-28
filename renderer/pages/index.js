3fe6575d62cf8f6fdde1d01088b3511a205853ee
You Song Hou <ysongweb3@gmail.com>
2023-03-28 19:35:15 -0400

    feat: integrate Auth Kit

--- renderer/pages/index.js
+++ renderer/pages/index.js
@@ -1,53 +1,116 @@
 import { useState, useEffect } from 'react';
 import { ethers } from 'ethers';
 
-import { GaslessOnboarding } from "@gelatonetwork/gasless-onboarding";
-import { GASLESSWALLET_KEY, RPC } from '../keys';
+// import { GaslessOnboarding } from "@gelatonetwork/gasless-onboarding";
+// import { GASLESSWALLET_KEY, RPC } from '../keys';
+
+import { SafeAuthKit, SafeAuthProviderType } from '@safe-global/auth-kit'
+import { WEB3AUTH_CLIENT_ID, RPC } from '../keys';
 
 const Home = () => {
   const [walletAddress, setWalletAddress] = useState();
   const [balance, setBalance] = useState();
-  const [gobMethod, setGOBMethod] = useState(null);
+  const [safeAuthSignInResponse, setSafeAuthSignInResponse] = useState(
+    null
+  );
+  const [safeAuth, setSafeAuth] = useState();
+  const [provider, setProvider] = useState(null);
+
+  // const [gobMethod, setGOBMethod] = useState(null);
   
+  // const login = async () => {
+  //   try{
+  //     const gaslessWalletConfig = { apiKey: GASLESSWALLET_KEY };
+  //     const loginConfig = {
+  //       domains: [window.location.origin],
+  //       chain: {
+  //         // id: 5,
+  //         // rpcUrl: RPC,
+  //         id: 80001,
+  //         rpcUrl: "https://rpc-mumbai.maticvigil.com/",
+  //       }
+  //     };
+  //     const gaslessOnboarding = new GaslessOnboarding(
+  //       loginConfig,
+  //       gaslessWalletConfig
+  //     );
+
+  //     await gaslessOnboarding.init();
+  //     setGOBMethod(gaslessOnboarding);
+
+  //     const web3AuthProvider = await gaslessOnboarding.login();
+  //     console.log("web3AuthProvider", web3AuthProvider);
+
+  //     const gaslessWallet = gaslessOnboarding.getGaslessWallet();
+  //     setWalletAddress(gaslessWallet.getAddress());
+
+  //     const signer = new ethers.providers.Web3Provider(web3AuthProvider);
+  //     console.log(signer);
+
+  //     const balance = await signer.getBalance(gaslessWallet.getAddress());
+  //     setBalance(balance.toString());
+  //   }
+  //   catch(error){
+  //     console.log(error);
+  //   }
+  // }
+
+  useEffect(() => {
+    ;(async () => {
+      setSafeAuth(
+        await SafeAuthKit.init(SafeAuthProviderType.Web3Auth, {
+          chainId: '0x13881',
+          authProviderConfig: {
+            rpcTarget: RPC, // Add your RPC e.g. https://goerli.infura.io/v3/<your project id>
+            clientId: WEB3AUTH_CLIENT_ID, // Add your client id. Get it from the Web3Auth dashboard
+            network: 'testnet' | 'mainnet', // The network to use for the Web3Auth modal. Use 'testnet' while developing and 'mainnet' for production use
+            theme: 'light' | 'dark', // The theme to use for the Web3Auth modal
+            modalConfig: {
+              // The modal config is optional and it's used to customize the Web3Auth modal
+              // Check the Web3Auth documentation for more info: https://web3auth.io/docs/sdk/web/modal/whitelabel#initmodal
+            }
+          }
+        })
+      )
+    })()
+  }, [])
+
   const login = async () => {
     try{
-      const gaslessWalletConfig = { apiKey: GASLESSWALLET_KEY };
-      const loginConfig = {
-        domains: [window.location.origin],
-        chain: {
-          // id: 5,
-          // rpcUrl: RPC,
-          id: 80001,
-          rpcUrl: "https://rpc-mumbai.maticvigil.com/",
-        }
-      };
-      const gaslessOnboarding = new GaslessOnboarding(
-        loginConfig,
-        gaslessWalletConfig
-      );
+      if (!safeAuth) return;
 
-      await gaslessOnboarding.init();
-      setGOBMethod(gaslessOnboarding);
+      const response = await safeAuth.signIn();
+      console.log('SIGN IN RESPONSE: ', response);
+      setWalletAddress(response.eoa);
+  
+      setSafeAuthSignInResponse(response);
+      setProvider(safeAuth.getProvider());
 
-      const web3AuthProvider = await gaslessOnboarding.login();
-      console.log("web3AuthProvider", web3AuthProvider);
-
-      const gaslessWallet = gaslessOnboarding.getGaslessWallet();
-      setWalletAddress(gaslessWallet.getAddress());
-
-      const signer = new ethers.providers.Web3Provider(web3AuthProvider);
+      const provider = new ethers.providers.Web3Provider(safeAuth.getProvider());
+      const signer = provider.getSigner();
       console.log(signer);
 
-      const balance = await signer.getBalance(gaslessWallet.getAddress());
+      const balance = await provider.getBalance(response.eoa);
+      console.log(balance);
       setBalance(balance.toString());
     }
     catch(error){
-      console.log(error);
+      console.error(error);
     }
   }
 
   const logout = async () => {
-    await gobMethod.logout();
+    try{
+      if (!safeAuth) return;
+
+      await safeAuth.signOut();
+
+      setProvider(null);
+      setSafeAuthSignInResponse(null);
+    }
+    catch(error){
+      console.error(error);
+    }
   }
 
 
