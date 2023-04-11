ce81b04ac5a85e9cb4d26a230952bf81995f9dc7
You Song Hou <ysongweb3@gmail.com>
2023-04-11 01:27:23 -0400

    feat: add a Select component for choosing a network

--- renderer/pages/index.js
+++ renderer/pages/index.js
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useRouter } from 'next/router';
-import { Typography ,Button } from 'antd';
+import { Typography, Select, Button } from 'antd';
+
+const NETWORK_LIST = [
+  {
+    value: 'polygon',
+    label: 'Polygon Mumbai',
+  },
+  {
+    value: 'sepolia',
+    label: 'Sepolia',
+  },
+]
 
 const Home = () => {
   const router = useRouter();
+ 
+  const [networkName, setNetworkName] = useState(null);
+
+  const handleChange = (value) => {
+    setNetworkName(value);
+  };
+
+  console.log(networkName);
 
   return (
     <center>
       <Typography.Title level={2} style={{ marginTop: '10rem', marginBottom: '2rem' }}>
         Desktop Safe Wallet
       </Typography.Title>
+      <Select
+        placeholder="Select a Network"
+        style={{
+          width: 120,
+        }}
+        onChange={handleChange}
+        options={NETWORK_LIST}
+      />
+      <br />
+      <br />
       <Button onClick={() => router.push('/dashboard')} type="primary" size='large'>
         Login
       </Button>
