2f00b7079d46684a3e7776f46c83cff62c3b8e11
You Song Hou <ysongweb3@gmail.com>
2023-04-10 22:06:47 -0400

    fix: wait for the instance of Safe Auth to be created

--- renderer/pages/dashboard.js
+++ renderer/pages/dashboard.js
@@ -37,32 +37,29 @@ const Dashboard = () => {
   const [enterOwners, setEnterOwners] = useState([]);
 
   useEffect(() => {
-    ;(async () => {
-      setSafeAuth(
-        await SafeAuthKit.init(SafeAuthProviderType.Web3Auth, {
-          chainId: '0x13881',
-          authProviderConfig: {
-            rpcTarget: RPC, // Add your RPC e.g. https://goerli.infura.io/v3/<your project id>
-            clientId: WEB3AUTH_CLIENT_ID, // Add your client id. Get it from the Web3Auth dashboard
-            network: 'testnet' | 'mainnet', // The network to use for the Web3Auth modal. Use 'testnet' while developing and 'mainnet' for production use
-            theme: 'light' | 'dark', // The theme to use for the Web3Auth modal
-            modalConfig: {
-              // The modal config is optional and it's used to customize the Web3Auth modal
-              // Check the Web3Auth documentation for more info: https://web3auth.io/docs/sdk/web/modal/whitelabel#initmodal
-            }
-          }
-        })
-      );
-      login();
-    })()
+    createInstanceAuth();
   }, [])
 
-  useEffect(() => {
-    if(safeAuth) login();
-   }, [safeAuth])
+  const createInstanceAuth = async () => {
+    const safeAuthKit = await SafeAuthKit.init(SafeAuthProviderType.Web3Auth, {
+      chainId: '0x13881',
+      authProviderConfig: {
+        rpcTarget: RPC, // Add your RPC e.g. https://goerli.infura.io/v3/<your project id>
+        clientId: WEB3AUTH_CLIENT_ID, // Add your client id. Get it from the Web3Auth dashboard
+        network: 'testnet' | 'mainnet', // The network to use for the Web3Auth modal. Use 'testnet' while developing and 'mainnet' for production use
+        theme: 'light' | 'dark', // The theme to use for the Web3Auth modal
+        modalConfig: {
+          // The modal config is optional and it's used to customize the Web3Auth modal
+          // Check the Web3Auth documentation for more info: https://web3auth.io/docs/sdk/web/modal/whitelabel#initmodal
+        }
+      }
+    });
+    setSafeAuth(safeAuthKit);
+    await login(safeAuthKit);
+  }
 
-  const login = async () => {
-    const { address, providerSafe, userSigner, ethbalance } = await loginSafe(safeAuth);
+  const login = async (safeAuthKit) => {
+    const { address, providerSafe, userSigner, ethbalance } = await loginSafe(safeAuthKit);
     console.log(address, providerSafe, userSigner, ethbalance)
 
     setWalletAddress(address);
@@ -74,7 +71,7 @@ const Dashboard = () => {
   }
 
   const logout = async () => {
-    await logoutSafe(safeAuth);
+    //await logoutSafe(safeAuth);
     setProvider(null);
     router.push('/')
   }
