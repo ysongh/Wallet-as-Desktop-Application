072caa9900f087fedad7b62e46e60dae8d75a4fe
You Song Hou <ysongweb3@gmail.com>
2023-04-30 17:53:37 -0400

    feat: update UI for Streaming section

--- renderer/components/Streaming.js
+++ renderer/components/Streaming.js
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react';
-import { Button, Form, Input, Typography } from 'antd';
+import { Tabs, Button, Form, Input, Typography } from 'antd';
 
 import { getDAIBalance, getfDAIxBalance, approveDAITokens, upgradeDAIToDAIx, streamDAIx, transferDAITokens } from '../utils/superfluid';
 
 const Streaming = ({ sfSdk, signer, walletAddress, messageApi }) => {
+  const [currentTab, setCurrentTab] = useState("1");
   const [amount, setAmount] = useState();
   const [loading, setLoading] = useState(false);
   const [fdaixbalance, setFdaixbalance] = useState(0);
@@ -12,10 +13,25 @@ const Streaming = ({ sfSdk, signer, walletAddress, messageApi }) => {
   const [to, setTo] = useState();
   const [amountToStream, setAmountToStream] = useState(0);
 
+  const items = [
+    {
+      key: '1',
+      label: `Stream`
+    },
+    {
+      key: '2',
+      label: `Upgrade`
+    },
+  ];
+
   useEffect(() => {
     getDAI();
     getfDAIx();
   }, [])
+
+  const changeTab = (key) => {
+    setCurrentTab(key);
+  };
   
   const getfDAIx = async() => {
     const balance = await getfDAIxBalance(sfSdk, signer, walletAddress);
@@ -69,51 +85,47 @@ const Streaming = ({ sfSdk, signer, walletAddress, messageApi }) => {
 
   return (
     <div>
-      <Typography.Title level={2}>
-        Streaming
-      </Typography.Title>
+      <Tabs defaultActiveKey="1" items={items} onChange={changeTab} />
+
       <Typography.Title level={4}>
         Balance
       </Typography.Title>
       <p>{daiBalance} DAI</p>
       <p>{fdaixbalance} fDAIx</p>
-      <Form layout="vertical">
-        <Form.Item label="Amount to upgrade DAI to DAIx">
-          <Input placeholder="0" value={amount} onChange={(e) => setAmount(e.target.value)}/>
-        </Form.Item>
+     
+      { currentTab === "1" && <>
+        <Form layout="vertical">
+          <Form.Item label="Address to stream">
+            <Input placeholder="0x0" value={to} onChange={(e) => setTo(e.target.value)}/>
+          </Form.Item>
 
-        <Button onClick={approveDAI} type="primary" disabled={!amount || isApprove} loading={loading}>
-          Approve
-        </Button>
-        
-        <Button onClick={updateDAI} type="primary" disabled={!amount || !isApprove} loading={loading}>
-          Upgrade
-        </Button>
-      </Form>
+          <Form.Item label="Flow Rate">
+            <Input placeholder="0" value={amountToStream} onChange={(e) => setAmountToStream(e.target.value)}/>
+          </Form.Item>
 
-      <br />
-      <br />
+          <Button onClick={stream} type="primary" disabled={!to || !amountToStream} loading={loading}>
+            Stream
+          </Button>
+          <Button onClick={() => transferDAITokens(sfSdk, signer, amountToStream, to)} type="primary" loading={loading}>
+            Transfer
+          </Button>
+        </Form>
+      </>}
+      { currentTab === "2" && <>
+        <Form layout="vertical">
+          <Form.Item label="Amount to upgrade DAI to DAIx">
+            <Input placeholder="0" value={amount} onChange={(e) => setAmount(e.target.value)}/>
+          </Form.Item>
 
-      <Typography.Title level={4}>
-        Stream
-      </Typography.Title>
-
-      <Form layout="vertical">
-        <Form.Item label="Address to stream">
-          <Input placeholder="0x0" value={to} onChange={(e) => setTo(e.target.value)}/>
-        </Form.Item>
-
-        <Form.Item label="Flow Rate">
-          <Input placeholder="0" value={amountToStream} onChange={(e) => setAmountToStream(e.target.value)}/>
-        </Form.Item>
-
-        <Button onClick={stream} type="primary" disabled={!to || !amountToStream} loading={loading}>
-          Stream
-        </Button>
-        <Button onClick={() => transferDAITokens(sfSdk, signer, amountToStream, to)} type="primary" loading={loading}>
-          Transfer
-        </Button>
-      </Form>
+          <Button onClick={approveDAI} type="primary" disabled={!amount || isApprove} loading={loading}>
+            Approve
+          </Button>
+          
+          <Button onClick={updateDAI} type="primary" disabled={!amount || !isApprove} loading={loading}>
+            Upgrade
+          </Button>
+        </Form>
+      </>}
     </div>
   )
 }
