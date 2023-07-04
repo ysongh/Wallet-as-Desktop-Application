c4c62221b8e0240ec57cdd160bfbab9954d2cbd6
You Song Hou <ysongweb3@gmail.com>
2023-07-03 22:15:38 -0400

    feat: add URLs to the explorer

--- renderer/network.js
+++ renderer/network.js
@@ -5,12 +5,14 @@ export const NETWORK = {
     "chainId": "0x13881",
     "rpc": POLYGON_RPC,
     "networkName": "Polygon Testnet",
-    "tokenSymbol": "MATIC"
+    "tokenSymbol": "MATIC",
+    "explorer": "https://mumbai.polygonscan.com/"
   },
   "chiado": {
     "chainId": "0x27d8",
     "rpc": "https://rpc.eu-central-2.gateway.fm/v4/gnosis/archival/chiado",
     "networkName": "Chiado Testnet",
-    "tokenSymbol": "Chiado xDai"
+    "tokenSymbol": "Chiado xDai",
+    "explorer": "https://blockscout.chiadochain.net/"
   }
 }
